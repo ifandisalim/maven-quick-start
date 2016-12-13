95cc3e5a9cc2b925592df19823ca08ee4211fbe8
Ifandi <ifandisalim@gmail.com>
2016-12-13 23:19:48 +0800

    changing compiler plugin to JDK 1.8

--- src/main/java/ifandi/programming/training/Application.java
+++ src/main/java/ifandi/programming/training/Application.java
@@ -1,13 +1,24 @@
 package ifandi.programming.training;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
 
     public Application() {
         System.out.println ("Inside Application");
     }
 
+
+
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
+      List<String> listSample = new ArrayList<>();
+      listSample.add("Hi");
+
+      for(String currentItem : listSample){
+        System.out.println(currentItem);
+      }
     	System.out.println ("Starting Application");
 	Application app = new Application();
     }
