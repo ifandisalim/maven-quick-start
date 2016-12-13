143b9c6554015e01a3954cfd74f53fba040afdd7
Ifandi <ifandisalim@gmail.com>
2016-12-13 23:37:50 +0800

    Used stringtuil and added dependency to lang3

--- src/main/java/ifandi/programming/training/Application.java
+++ src/main/java/ifandi/programming/training/Application.java
@@ -2,6 +2,7 @@ package ifandi.programming.training;
 
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
 
@@ -9,17 +10,20 @@ public class Application {
         System.out.println ("Inside Application");
     }
 
+    public int countWords(String words){
+      String[] separateWords = StringUtils.split(words, ' ');
+      return (saparateWords == null) ? 0 : separateWords.length;
+    }
+
 
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-      List<String> listSample = new ArrayList<>();
-      listSample.add("Hi");
-
-      for(String currentItem : listSample){
-        System.out.println(currentItem);
-      }
     	System.out.println ("Starting Application");
-	Application app = new Application();
+
+	    Application app = new Application();
+
+      int count = app.countWords("I have four words");
+      System.out.println("Word Count " + count);
     }
 }
